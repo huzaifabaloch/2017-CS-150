da36ef677d48cf16ede8ffe332e8c69093410cdd
Huzaifa Baloch <huzbaudi3@gmail.com>
2018-01-29 02:23:04 +0500

    LAB 01 TASKS ADDED

---
+++ Lab_01/Lab_01/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+/*
+        ============================================================================================
+                                            LAB # 01 
+        ============================================================================================
+
+ * 
+ * 
+ *    PROGRAM TO CALCULATE THE SUM OF TWO INTEGER NUMBERS
+ * 
+ * 
+        int numberone;
+        int numbertwo;
+        int sum;
+
+        console.writeline("enter your first number");
+        numberone = convert.toint32(console.readline());
+
+        console.writeline("enter your second number");
+        numbertwo = convert.toint32(console.readline());
+
+        sum = numberone + numbertwo;
+
+        console.writeline("\nthe sum of {0} & {1} is {2}", numberone, numbertwo, sum);
+        console.readkey();
+
+        ================================================================================================
+
+ * 
+ * 
+ * 
+ *      PROGRAM TO FIND EVEN AND ODD
+ *      
+        int number;
+        string result;
+
+        Console.WriteLine("Enter a number to check whether it is Even or Odd");
+        number = int.Parse(Console.ReadLine());
+
+        result = ((number % 2) != 0 ? "ODD" : "EVEN");
+
+        Console.WriteLine("\nThe number, {0} is {1}", number, result);
+        Console.ReadKey();
+
+        ================================================================================================
+
+ * 
+ *      PROGRAM TO SWAP BW TWO INTEGER NUMBERS
+ * 
+        int firstNumber;
+        int secondNumber;
+
+        Console.WriteLine("Enter your first number");
+        firstNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter your second number");
+        secondNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("=========BEFORE SWAPPING========");
+        Console.WriteLine("First Number = {0}", firstNumber);
+        Console.WriteLine("Second Number = {0}", secondNumber);
+
+        firstNumber = firstNumber + secondNumber;
+        secondNumber = firstNumber - secondNumber;
+        firstNumber = firstNumber - secondNumber;
+
+        Console.WriteLine("=========AFTER SWaPPING=========");
+        Console.WriteLine("First Number = {0}", firstNumber);
+        Console.WriteLine("Second Number = {0}", secondNumber);
+
+        Console.ReadKey();
+
+
+        =================================================================================================
+
+ * 
+ *      PROGRAM TO FIND THE FACTORIAL
+ * 
+ * 
+        int factorial = 1;
+        int number;
+        int u;
+
+        Console.WriteLine("Enter Your Number to see its factorial");
+        number = int.Parse(Console.ReadLine());
+
+        for (u = 1; u <=number; u++) {
+
+            factorial = factorial * u;
+
+        }
+
+        Console.WriteLine("The Factorial of {0} is, {1}", number, factorial);
+        Console.ReadKey();
+
+        ==================================================================================================
+
+ * 
+ *      PROGRAM FOR FIBONACCI SERIES
+ * 
+ * 
+
+        int fNum = 1;
+        int sNum = 1;
+        int number;
+        int track;
+        int f;
+
+
+        Console.WriteLine("Enter Your fibonacci serie number");
+        number = Convert.ToInt32(Console.ReadLine());
+
+        for (f = 0; f < number; f++)
+        {
+            track = fNum + sNum;
+            fNum = sNum;
+            sNum = track;
+
+            Console.Write("{0}, ", fNum);
+           
+        }
+        Console.ReadKey();
+
+
+        //=================================================================================================
+
+   
+
+    * 
+    * 
+    * */
+
+}
+
+}
+
