e2927fe614aa5b58ee17fe05edafac6f9a947b7b
huzaifabaloch <husudbs.hiajn>
2018-01-30 02:18:12 +0500

    LAB 02 And LAB 03 Tasks added

---
+++ Lab_02/Lab_02/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+
+class Program
+    {
+
+    public static double Addition(double fnum, double snum) {
+
+        return fnum + snum;
+    }
+
+    public static double Subtraction(double fnum, double snum) {
+
+        return fnum - snum;
+    }
+
+    public static double Multiplication(double fnum, double snum)
+    {
+
+        return fnum * snum;
+    }
+
+
+    public static double Division(double fnum, double snum)
+    {
+
+        return fnum / snum;
+    }
+
+    public static double InputOne() {
+
+        double fnum;
+
+        Console.WriteLine("\nEnter the first number");
+        fnum = Convert.ToDouble(Console.ReadLine());
+
+        return fnum;
+    
+    }
+
+    public static double InputTwo()
+    {
+        double snum; 
+
+        Console.WriteLine("\nEnter the second number");
+        snum = Convert.ToDouble(Console.ReadLine());
+
+        return snum;
+    }
+
+    static void Main(string[] args)  {
+
+        double firstNumber = 0;
+        double secondNumber = 0;
+        double answer;
+        int userOption;
+        char exit;
+
+        do {
+        Console.Clear();
+        Console.WriteLine("\n======CALCULATOR======");
+        Console.WriteLine("\n1. Addition");
+        Console.WriteLine("2. Subtraction");
+        Console.WriteLine("3. Multiplication");
+        Console.WriteLine("4. Division");
+        Console.WriteLine("5. Power");
+        Console.WriteLine("6. Square Root");
+        Console.WriteLine("7. Logarithm");
+        Console.WriteLine("\nSelect Your Operation?");
+        Console.WriteLine("--------------------------"); 
+        userOption = int.Parse(Console.ReadLine());
+
+
+        switch (userOption) { 
+ 
+
+            case 1:
+
+                firstNumber = InputOne();
+                secondNumber = InputTwo();
+                answer = Addition(firstNumber, secondNumber);
+                Console.WriteLine("\n\tThe Result of Addtion, {0}", answer);
+                break;
+
+
+            case 2:
+
+                firstNumber = InputOne();
+                secondNumber = InputTwo();
+                answer = Subtraction(firstNumber, secondNumber);
+                Console.WriteLine("\n\tThe Result of Subtraction, {0}", answer);
+
+                break;
+
+            case 3:
+
+                firstNumber = InputOne();
+                secondNumber = InputTwo();
+                answer = Multiplication(firstNumber, secondNumber);
+                Console.WriteLine("\n\tThe Result of Multiplication, {0}", answer);
+                break;
+
+
+            case 4:
+
+                firstNumber = InputOne();
+                secondNumber = InputTwo();
+                answer = Division(firstNumber, secondNumber);
+                Console.WriteLine("\n\tThe Result of Division, {0}", answer);
+                break;
+
+            case 5:
+
+                firstNumber = InputOne();
+                Console.WriteLine("\nEnter the value of power");
+                secondNumber = Convert.ToInt32(Console.ReadLine());
+                answer = Math.Pow(firstNumber, secondNumber);
+                Console.WriteLine("\n\tThe {0} times {1} is {2}", secondNumber, firstNumber, answer);
+                break;
+
+
+            case 6:
+
+                Console.WriteLine("\nEnter the number for square root");
+                firstNumber = Convert.ToInt32(Console.ReadLine());
+                answer = Math.Sqrt(firstNumber);
+                Console.WriteLine("\n\tThe Square Root of {0} is {1}", firstNumber, answer);
+                break;
+
+            case 7:
+
+                Console.WriteLine("\nEnter the number for log");
+                firstNumber = Convert.ToInt32(Console.ReadLine());
+                answer = Math.Log(firstNumber);
+                Console.WriteLine("\n\tThe Log of {0} is {1}", firstNumber, answer);
+                break;
+        }
+
+
+        Console.ReadKey();
+
+        Console.Clear();
+        Console.WriteLine("CONTINUE USING EXTRA SMART CALCULATOR?");
+        exit = char.Parse(Console.ReadLine());
+
+        }while(exit != 'N'); 
+}
+
+
+    }
---
+++ Lab_03/Lab_03/Program.cs
@@ -0,0 +1,189 @@
+﻿using System;
+
+
+
+
+/*
+
+class Student
+{
+
+    public string name;
+    public byte age;
+    public int mrksOfEng;
+    public int mrksOfMaths;
+    public int mrksOfSci;
+    public int totalMarks;
+    public int obtainedMarks;
+    public float perCentage;
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student STD = new Student();
+
+        int Total;
+        float perCent;
+ 
+        Console.WriteLine("\nEnter Your Name");
+        STD.name = Console.ReadLine();
+
+        Console.WriteLine("\nEnter Your Age");
+        STD.age = Convert.ToByte(Console.ReadLine());
+
+        Console.WriteLine("\nEnter Your Marks in English");
+        STD.mrksOfEng = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("\nEnter Your Marks in Mathematics");
+        STD.mrksOfMaths = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("\nEnter Your Marks in Science");
+        STD.mrksOfSci = Convert.ToInt32(Console.ReadLine());
+
+        Total = Total_Marks(STD.mrksOfEng, STD.mrksOfMaths, STD.mrksOfSci);
+
+        perCent = Percentage_I(Total);
+
+        Console.Clear();
+        Console.WriteLine("=======================================");
+        Console.WriteLine("\t\tSTUDENT RESULT");
+        Console.WriteLine("=======================================");
+        Console.WriteLine("Name = {0}",STD.name);
+        Console.WriteLine("Age = {0}", STD.age);
+        Console.WriteLine("English = {0}", STD.mrksOfEng);
+        Console.WriteLine("Mathematics = {0}", STD.mrksOfMaths);
+        Console.WriteLine("Science = {0}", STD.mrksOfSci);
+        Console.WriteLine("Total Marks = {0}", Total);
+        Console.WriteLine("Percentage = {0}", perCent);
+        Console.ReadKey();
+
+    }
+
+
+    public static int Total_Marks(int eng, int maths, int sci) {
+
+        Student STD = new Student();
+
+        STD.obtainedMarks = eng + maths + sci;
+
+        return STD.obtainedMarks;
+
+    }
+
+    public static float Percentage_I(int obtained) {
+        
+        Student STD = new Student();
+        STD.totalMarks = 300;
+
+        STD.perCentage = ((float)obtained / STD.totalMarks) * 100;
+
+        return STD.perCentage;
+
+        
+        
+    }
+}
+*/
+
+
+
+
+class Program {
+
+    static void Main(string[] args)
+    {
+
+        string name;
+        int vowels = 0;
+        int storeLen;
+
+        Console.WriteLine("Enter a name to see vowels in it");
+        name = Console.ReadLine();
+
+        storeLen = name.Length;
+
+        int i;
+
+        for (i = 0; i < storeLen; i++)
+        {
+            if(name[i] == 'a' || name[i] == 'e' || name[i] == 'i' || name[i] == 'o' || name[i] == 'u' ||
+                name[i] == 'A' || name[i] == 'E' || name[i] == 'I' || name[i] == 'O' || name[i] == 'U')
+            {
+                vowels++;
+            }
+        }
+
+
+        Console.WriteLine("The number of vowels in {0} are {1}", name, vowels);
+        Console.ReadKey();
+
+
+    }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+/*
+class Rectangle {
+
+    private double length;
+    private double width;
+
+    public double Perimeter_Rectangle(double l, double w) {
+
+        length = l;
+        width = w;
+        return 2 * (length + width);
+
+    }
+
+    public double Area_of_Rectangle(double l, double w) {
+
+        length = l;
+        width = w;
+        return width * length;
+    
+    }
+}
+
+
+class Program {
+
+    static void Main(string[] args) {
+
+        Rectangle rect = new Rectangle();
+
+        double perimeter;
+        double area;
+        double len;
+        double wid;
+
+        Console.WriteLine("Enter the lenght");
+        len = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter the width");
+        wid = Convert.ToDouble(Console.ReadLine());
+
+        perimeter = rect.Perimeter_Rectangle(len, wid);
+        area = rect.Area_of_Rectangle(len, wid);
+
+        Console.WriteLine(perimeter);
+        Console.WriteLine(area);
+        Console.ReadKey();
+     
+    }
+}
+*/
+
